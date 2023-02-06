3e1b53e44283eb377beac87719d03b9444fd9f63
Aleksandr <trololo1009@gmail.com>
2023-02-06 22:26:27 +0500

    added Sidebar component

---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -0,0 +1,28 @@
+import { FC, useState } from "react";
+import { classNames } from "shared/lib/classNames/classNames";
+import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
+import cls from "./Sidebar.module.scss";
+
+interface SidebarProps {
+  className?: string;
+}
+
+export const Sidebar: FC<SidebarProps> = (props) => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const onToggle = () => {
+    setCollapsed((prev) => !prev);
+  };
+
+  const { className } = props;
+
+  return (
+    <div className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}>
+      <button onClick={onToggle}>TOGGLE</button>
+      <div className={cls.switchers}>
+        <ThemeSwitcher />
+        {/* <LangSwithcer /> */}
+      </div>
+    </div>
+  );
+};
